95ad3b8e72bc6a7d903088d65ee15bd606d4aa05
Ezra Deutsch <ezdeutsch@gmail.com>
2026-07-18 12:46:38 -0400

    streamlit addition

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import streamlit as st
 
 
 def main():
-    st.header()
+    st.header("Hello, welcome to my massive-pipeline project!")
 
+    value_1 = st.number_input("Value 1")
+    value_2 = st.number_input("Value 2")
+
+    st.write(f"The sum of {value_1} + {value_2} is {value_1 + value_2}")
 
 
     print("Hello from massive-pipeline!")
